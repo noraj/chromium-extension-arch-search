360d377b2176c47785969a37574db1e1627f6086
noraj <alexandre.zanni@europe.com>
2025-04-19 23:59:49 +0200

    add version in filename

--- generate_zip.rb
+++ generate_zip.rb
@@ -8,16 +8,21 @@ end
 
 # Script
 require 'zip'
+require 'json'
 
 # overwritte exising files
 Zip.on_exists_proc = true
 Zip.continue_on_exists_proc = true
 
+# get version
+version = JSON.load_file('manifest.json')['version']
+
 input_filenames = Dir['_locales/**/*.json', 'images/**/*.png', 'background.js', 'manifest.json']
-zipfile_name = 'archlinux_search.zip'
+zipfile_name = "archlinux_search_#{version}.zip"
 
 Zip::File.open(zipfile_name, create: true) do |zipfile|
   input_filenames.each do |filename|
     zipfile.add(filename, File.join('./', filename))
   end
 end
+puts "[+] #{zipfile_name}"
